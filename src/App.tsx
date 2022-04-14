fbb175ea9b2bc876c945bc5a031bfed48cbdef4c
Нурбек Азамат уулу <nurbek@MacBook-Air-2.local>
2022-04-14 10:29:44 +0600

    removed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
 
 import './app.css';
 import Layout from './pages/layout/layout';
@@ -12,11 +7,7 @@ import MainPage from './pages/main-page/main-page';
 function App() {
   return (
     <Layout>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
+      <MainPage />
     </Layout>
   );
 }
